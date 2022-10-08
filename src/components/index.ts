836dc47967398068f3cb40d77b4898577df4fc95
Prajwal Kulkarni <prajwalkulkarni76@gmail.com>
2022-10-08 19:50:07 +0530

    Add new loading indicator - foursquare

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,6 +3,7 @@ import { CircularProgress } from "./CircularProgress";
 export * from "./Atom";
 export * from "./Commet";
 export * from "./CircularProgress";
+export * from "./FourSquare";
 export * from "./Mosaic";
 export * from "./Riple";
 export * from "./Seek";
