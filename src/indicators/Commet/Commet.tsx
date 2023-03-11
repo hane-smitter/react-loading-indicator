1acf4ce362401beaaed9fa6173af1d7194f0d38d
hane-smitter <hanesmitter3@gmail.com>
2023-03-11 23:37:38 +0300

    retouch commet animation Improved responsiveness Fix ringball misalignment

--- src/indicators/Commet/Commet.tsx
+++ src/indicators/Commet/Commet.tsx
@@ -1,94 +1,103 @@
 import React from "react";
+
 import { CommetProps } from "./Commet.types";
 import "./Commet.scss";
+import useFontsizeMapper from "../../hooks/useFontsizeMapper";
 
 const Commet = (props: CommetProps) => {
-  // Styles
-  let styles: React.CSSProperties = props?.style || {};
+	// Styles
+	let styles: React.CSSProperties = Object(props?.style);
 
-  // color SETTINGS
-  // If Color property is a string, that is the color of all rings
-  // If color property is an array, that is color for each rings
-  let allRingsColor: { color?: string } = {};
-  let ringColorsArr: string[] = [];
-  let ring1Color: { color?: string } = {};
-  let ring2Color: { color?: string } = {};
-
-  if (props?.color && typeof props?.color === "string") {
-    allRingsColor = { color: props?.color };
-  } else if (props?.color?.length && props?.color instanceof Array) {
-    ringColorsArr = props?.color;
-  }
-  if (ringColorsArr.length > 0) {
-    const [ring1, ring2] = ringColorsArr;
-    ring1Color = {
-      ...(ring1 && { color: ring1 }),
-    };
-    ring2Color = {
-      ...(ring2 && { color: ring2 }),
-    };
-  }
-
-  // Sizes
-  const size: string = props?.size || "";
-  let fontSize: string | number = "";
-  switch (size) {
-    case "small":
-      fontSize = "12px";
-      break;
-    case "medium":
-      fontSize = "16px";
-      break;
-    case "large":
-      fontSize = "20px";
-      break;
-  }
-  // Setting size by specifying font-size in style attr
-  // and modifying styles to exclude fontSize
+	// Sizes
+	let fontSize: string | number = useFontsizeMapper(props?.size);
+	// Setting size by specifying font-size in style attr
+	// and modifying styles to exclude fontSize
 	if (props?.style?.fontSize) {
-		const { fontSize: extractedFontSize, ...extractedStyles } = props?.style;
+		const { fontSize: cssFontSize, ...css } = props?.style;
 
-    styles = extractedStyles;
-		fontSize = extractedFontSize;
+		styles = css;
+		fontSize = cssFontSize;
 	}
 
-  return (
-    <span
-      className="rli-d-i-b commet-bounding-box"
-      style={{ ...(fontSize && { fontSize }) }}
-    >
-      <span
-        className="rli-d-i-b commet-loader"
-        style={{
-          ...allRingsColor,
-          ...styles,
-        }}
-      >
-        <span
-          className="rli-d-i-b commet-text"
-          style={{
-            ...(props?.textColor && {
-              color: props?.textColor,
-              mixBlendMode: "unset",
-            }),
-          }}
-        >
-          {props?.text
-            ? typeof props?.text === "string" && props?.text.length
-              ? props?.text
-              : "loading"
-            : null}
-        </span>
+	// color SETTINGS
+	// If Color property is a string, that is the color of all rings
+	// If color property is an array, that is color for each rings
+	let ringColors: string | string[] = props?.color ?? "";
+	const ringColorStyles: React.CSSProperties =
+		ringColors instanceof Array
+			? { ...genStyleFromColorArr(ringColors) }
+			: { ...genStyleFromColorStr(ringColors) };
 
-        <span className="rli-d-i-b ring ring1" style={{ ...ring1Color }}>
-          <span className="rli-d-i-b ringball"></span>
-        </span>
-        <span className="rli-d-i-b ring ring2" style={{ ...ring2Color }}>
-          <span className="rli-d-i-b ringball"></span>
-        </span>
-      </span>
-    </span>
-  );
+	return (
+		<span
+			className="rli-d-i-b commet-bounding-box"
+			style={{ ...(fontSize && { fontSize }) }}
+		>
+			<span
+				className="rli-d-i-b commet-loader"
+				style={{
+					...ringColorStyles,
+					...styles
+				}}
+			>
+				<span
+					className="rli-d-i-b rli-text-format commet-text"
+					style={{
+						...(props?.textColor && {
+							color: props?.textColor,
+							mixBlendMode: "unset"
+						})
+					}}
+				>
+					{props?.text
+						? typeof props?.text === "string" && props?.text.length
+							? props?.text
+							: "loading"
+						: null}
+				</span>
+
+				<span className="rli-d-i-b ring-wrapper ring1">
+					<span className="rli-d-i-b ring "></span>
+					<span className="rli-d-i-b ringball-holder"></span>
+				</span>
+				<span className="rli-d-i-b ring-wrapper ring2">
+					<span className="rli-d-i-b ring "></span>
+					<span className="rli-d-i-b ringball-holder"></span>
+				</span>
+				{/* <span className="rli-d-i-b ring ring2">
+					<span className="rli-d-i-b ringball"></span>
+				</span> */}
+			</span>
+		</span>
+	);
 };
 
 export { Commet };
+
+function genStyleFromColorStr(
+	colorStr: string | undefined
+): React.CSSProperties {
+	colorStr = colorStr ?? "";
+
+	const stylesObject: any = {};
+
+	stylesObject["color"] = colorStr;
+
+	return stylesObject;
+}
+
+function genStyleFromColorArr(colorArr: string[]): React.CSSProperties {
+	const stylesObject: any = {};
+	const arrLength = colorArr.length;
+
+	stylesObject["color"] = colorArr[0];
+
+	for (let idx = 0; idx < arrLength; idx++) {
+		if (idx >= 2) break;
+		let currentItem = `ring${idx + 1}`;
+
+		stylesObject[`--${currentItem}-color`] = colorArr[idx];
+	}
+
+	return stylesObject;
+}
