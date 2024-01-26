103928eebd0492955dba8a22bcb2c804d75a9496
smitter <hanesmitter3@gmail.com>
2024-01-26 14:53:04 +0300

    Clean up debugging comments in Orbit Progress variant:dotted

--- src/indicators/OrbitProgress/Dotted/Dotted.tsx
+++ src/indicators/OrbitProgress/Dotted/Dotted.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useRef, useEffect } from "react";
+import React, { useCallback, useRef } from "react";
 
 import "./Dotted.scss";
 import { DottedProps } from "./Dotted.types";
